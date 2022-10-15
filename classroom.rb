2b64568d5bf8cc533c55a87beda9dcd698cf0f3b
DiegoMero <diegomero_1999@hotmail.com>
2022-10-15 06:56:59 -0500

    Add many to many relationship

--- classroom.rb
+++ classroom.rb
@@ -6,7 +6,7 @@ class Classroom
     @students = []
   end
 
-  attr_accessor :label  
+  attr_accessor :label
   attr_reader :students
 
   def add_student(student)
@@ -14,15 +14,3 @@ class Classroom
     student.classroom = self
   end
 end
-
-student_1 = Student.new(20, 7, "Dieg  o")
-
-student_2 = Student.new(18, 7, "Maykol")
-
-lololol = Classroom.new(5)
-
-lololol.add_student(student_1)
-
-lololol.add_student(student_2)
-
-puts lololol.students[1].classroom
